d5ba820cfcddd8c92b5658012cfe5792fadfdd8c
Renan Camaforte <r.camaforte@docu-tools.com>
2019-02-11 15:15:36 +0100

    Fixes an issue which the progresshud wasn't being centered in the view after a keyboard appeared

--- ProgressHUD/ProgressHUD/ProgressHUD.m
+++ ProgressHUD/ProgressHUD/ProgressHUD.m
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2018 Related Code - http://relatedcode.com
+// Copyright (c) 2016 Related Code - http://relatedcode.com
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -309,29 +309,15 @@
 - (void)hudPosition:(NSNotification *)notification
 //-------------------------------------------------------------------------------------------------------------------------------------------------
 {
-	CGFloat heightKeyboard = 0;
-	NSTimeInterval duration = 0;
-	//---------------------------------------------------------------------------------------------------------------------------------------------
-	if (notification != nil)
-	{
-		NSDictionary *info = [notification userInfo];
-		CGRect keyboard = [[info valueForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
-		duration = [[info valueForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue];
-		if ((notification.name == UIKeyboardWillShowNotification) || (notification.name == UIKeyboardDidShowNotification))
-		{
-			heightKeyboard = keyboard.size.height;
-		}
-	}
-	else heightKeyboard = [self keyboardHeight];
-	//---------------------------------------------------------------------------------------------------------------------------------------------
-	CGRect screen = [UIScreen mainScreen].bounds;
-	CGPoint center = CGPointMake(screen.size.width/2, (screen.size.height-heightKeyboard)/2);
-	//---------------------------------------------------------------------------------------------------------------------------------------------
-	[UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
-		self->toolbarHUD.center = CGPointMake(center.x, center.y);
-	} completion:nil];
-	//---------------------------------------------------------------------------------------------------------------------------------------------
-	if (viewBackground != nil) viewBackground.frame = window.frame;
+    NSTimeInterval duration = 0;
+    CGRect screen = [UIScreen mainScreen].bounds;
+    CGPoint center = CGPointMake(screen.size.width/2, screen.size.height/2);
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    [UIView animateWithDuration:duration delay:0 options:UIViewAnimationOptionAllowUserInteraction animations:^{
+        toolbarHUD.center = CGPointMake(center.x, center.y);
+    } completion:nil];
+    //---------------------------------------------------------------------------------------------------------------------------------------------
+    if (viewBackground != nil) viewBackground.frame = window.frame;
 }
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------
@@ -378,8 +364,8 @@
 
 		UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseOut;
 		[UIView animateWithDuration:0.15 delay:0 options:options animations:^{
-			self->toolbarHUD.transform = CGAffineTransformScale(self->toolbarHUD.transform, 1/1.4, 1/1.4);
-			self->toolbarHUD.alpha = 1;
+			toolbarHUD.transform = CGAffineTransformScale(toolbarHUD.transform, 1/1.4, 1/1.4);
+			toolbarHUD.alpha = 1;
 		} completion:nil];
 	}
 }
@@ -392,8 +378,8 @@
 	{
 		UIViewAnimationOptions options = UIViewAnimationOptionAllowUserInteraction | UIViewAnimationCurveEaseIn;
 		[UIView animateWithDuration:0.15 delay:0 options:options animations:^{
-			self->toolbarHUD.transform = CGAffineTransformScale(self->toolbarHUD.transform, 0.7, 0.7);
-			self->toolbarHUD.alpha = 0;
+			toolbarHUD.transform = CGAffineTransformScale(toolbarHUD.transform, 0.7, 0.7);
+			toolbarHUD.alpha = 0;
 		}
 		completion:^(BOOL finished) {
 			[self hudDestroy];
